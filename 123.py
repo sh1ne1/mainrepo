5521fe94c3ecd9f3e99eb1054030bdb31378f4da
Arseniy <mraksheidin@gmail.com>
2021-03-26 23:23:27 +0200

    for_reve

---
+++ 123.py
@@ -0,0 +1,16 @@
+a = []
+ch = 2
+while len(a) != 100:
+    a.append(str(ch))
+    ch += 25
+for i in a:
+    if not i[-1] == '2':
+        a.remove(i)
+for j in range(len(ad)):
+    if '0' in a[j]:
+        kek = a[j]
+        e.split('1')
+        kek = ''.join(kek)
+        a.insert(j, kek)
+        a.remove(a[j + 1])
+print(a)
