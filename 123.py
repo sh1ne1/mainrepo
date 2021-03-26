52e2fc60e9617ca387ba1ce90bb1b6f5a36adb4f
Arseniy <mraksheidin@gmail.com>
2021-03-26 22:55:25 +0200

    for_conf

---
+++ 123.py
@@ -0,0 +1,15 @@
+a = []
+ch = 2
+while len(a) != 100:
+    a.append(str(ch))
+    ch += 24
+for i in a:
+    if i[-1] == '2':
+        a.remove(i)
+for j in range(len(a)):
+    if '0' in a[j]:
+        kek = a[j].split('0')
+        kek = ''.join(kek)
+        a.insert(j, kek)
+        a.remove(a[j+1])
+print(a)
